9fe1ad4f2529fbebe7c36f44bfe4e2f94ce8072a
@vohoaiduc.cr7 <vohoaiduc.cr7@gmail.com>
2023-03-23 08:28:28 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function print() {
   const elementHTML = document.getElementById("contentPDF");
   const options = {
-    margin: [0.1, 0.1, 0.1, 0.1],
+    margin: [0.01, 0.01, 0.01, 0.01],
     filename: "myfile.pdf",
     image: { type: "jpeg", quality: 1 },
-    html2canvas: { scale: 1.5 },
+    html2canvas: { scale: 1 },
     jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
   };
   html2pdf().set(options).from(elementHTML).save();
