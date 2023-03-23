0c02e49436719cc01bb5125c72de94b57c1cc8ca
@vohoaiduc.cr7 <vohoaiduc.cr7@gmail.com>
2023-03-23 14:40:16 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-function print() {
-  const elementHTML = document.getElementById("contentPDF");
-  const options = {
-    margin: [0.01, 0.01, 0.01, 0.01],
-    filename: "myfile.pdf",
-    image: { type: "jpeg", quality: 1 },
-    html2canvas: { scale: 1 },
-    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-  };
-  html2pdf().set(options).from(elementHTML).save();
+// function savePDF() {
+//   const elementHTML = document.getElementById("contentPDF");
+//   const options = {
+//     margin: [0.01, 0.01, 0.01, 0.01],
+//     filename: "myfile.pdf",
+//     image: { type: "jpeg", quality: 1 },
+//     html2canvas: { scale: 1 },
+//     jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+//   };
+//   html2pdf().set(options).from(elementHTML).save();
+// }
+// const buttonSavePDF = document.getElementById("savePDF");
+// buttonSavePDF.onclick = savePDF;
+
+function toPDF() {
+  window.open(
+    "https://drive.google.com/file/d/1bPkrz7Bn1Zj1prOIAR4QF6oHOLHypODI/view",
+    "_blank"
+  );
 }
-const buttonSavePDF = document.getElementById("savePDF");
-buttonSavePDF.onclick = print;
+const buttontoPDF = document.getElementById("toPDF");
+buttontoPDF.onclick = toPDF;
