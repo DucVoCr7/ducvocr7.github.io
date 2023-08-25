fa014884c90de782539393604aeabca29ee7d775
@vohoaiduc.cr7 <vohoaiduc.cr7@gmail.com>
2023-08-25 12:54:09 +0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-// window.jsPDF = window.jspdf.jsPDF;
-
 const downloadPDF = ()=> {
-    // window.html2canvas = html2canvas;
-    // const doc = new jsPDF();
-    // const elementHTML = document.querySelector(".container");
-    // doc.html(elementHTML, {
-    //     callback: function(doc) {
-    //         doc.save('sample-document.pdf');
-    //     },
-    //     x: 15,
-    //     y: 15,
-    //     width: 170, //target width in the PDF document
-    //     windowWidth: 650 //window width in CSS pixels
-    // });
-
+    const link=document.createElement('a');
+    const filePath = './vohoaiduc_CV.pdf'
+    link.href = filePath;
+    link.download = filePath.substr(filePath.lastIndexOf('/') + 1);
+    link.click();
 }
 
 const button = document.getElementById('download_pdf')
